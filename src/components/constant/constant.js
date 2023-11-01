74c5c1202cb1855a5fedf0ca9abc689729a634e4
Nidhi Dhiman <104757239+nidhidhiman-123@users.noreply.github.com>
2023-11-01 17:41:50 +0530

    Update constant.js

--- src/components/constant/constant.js
+++ src/components/constant/constant.js
@@ -1,6 +1,6 @@
-import { create } from 'zustand'
+// import { create } from 'zustand'
 
-export const useStore = create((set) => ({
-    count: 0,
-    increase: (val) => set((state) => ({ count:val })),
-  }))
+// export const useStore = create((set) => ({
+//     count: 0,
+//     increase: (val) => set((state) => ({ count:val })),
+//   }))

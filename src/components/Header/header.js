5c955b8f3d5abb809239a412da3634ccbf19227d
Nidhi Dhiman <104757239+nidhidhiman-123@users.noreply.github.com>
2023-11-01 18:17:31 +0530

    Update header.js

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,15 +2,7 @@ import React, { useState, useRef } from "react";
 import { useStore } from "../constant";
 import "./index.css";
 const Header = () => {
-  const { count } = useStore((state) => state);
-  const {
-    handleShowMenu,
-    showMenu,
-    handleShowCart,
-    showCart,
-    exploreModal,
-    handleItems,
-  } = useStore((state) => state);
+  const { count,handleShowMenu, showMenu,handleShowCart,showCart,exploreModal,handleItems} = useStore((state) => state);
   return (
     <>
       <div className="header-containers">
@@ -31,27 +23,68 @@ const Header = () => {
             />
           )}
           <img src="/gabar.png" alt="Gabar" className="logo-image" />
-          <div
-            style={{ position: "relative", opacity: !exploreModal ? 0 : 1 }}
-            onClick={() => {
-              if (exploreModal) {
-                handleShowCart(!showCart);
-                handleItems(false);
-              }
-            }}
-          >
+          <div style={{ position: "relative",opacity:!exploreModal?0:1 }} onClick={()=>{
+            if(exploreModal)
+            
+            { handleShowCart(!showCart) 
+            handleItems(false)
+          }}}>
             <div className="itemCount">
               <span className="counter-number">{count}</span>
             </div>
-            <img
-              src="/Vector.png"
-              style={{ height: 20, width: 20, opacity: !exploreModal ? 0 : 1 }}
-            />
+            <img src="/Vector.png" style={{ height: 20, width: 20,opacity:!exploreModal?0:1 }} />
           </div>
         </div>
       </div>
 
+      {showMenu && (
+        <div className="modalss">
+          <div className="modal-contentss">
+            <div className="items-option-container">
+              <span
+                className="option-text"
+                onClick={() => handleShowMenu(false)}
+              >
+                Gabar for Myanmar
+              </span>
+              <div className="bottom-border" />
+              <span
+                className="option-text"
+                onClick={() => handleShowMenu(false)}
+              >
+                Stores
+              </span>
+              <div className="bottom-border" />
+              <span
+                className="option-text"
+                onClick={() => handleShowMenu(false)}
+              >
+                About us
+              </span>
+              <div className="bottom-border" />
+              <div className="social-links-container">
+                <div className="social-links">
+                  <img src="/instagram.png" className="social-link-img" />
+                  {/* <span className="social-link-txt">/gabar</span> */}
+                </div>
+                <div className="social-links">
+                  <img
+                    src="/tiktok.png"
+                    alt="TikTok"
+                    className="social-link-img"
+                  />
+                  {/* <span className="social-link-txt">@gabarinthewild</span> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
+    // </div>
   );
 };
+
+
+
 export default Header;
